6679f6e70b0fceb6fbd4b1ec1fb6993c045f22cb
Armaan Esfahani <armaanaki@gmail.com>
2019-09-10 15:17:39 -0400

    prints table of correct n and p values
    Signed-off-by: Armaan Esfahani <armaanaki@gmail.com>

--- assign3.py
+++ assign3.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import math
+from prettytable import PrettyTable
 
 # method to return Tserial
 def Tserial(n):
@@ -12,11 +13,37 @@ def Tparallel(n, p):
 
 # method to calculate speedup
 def speedup(n, p):
-    return Tserial(n)/Tparallel(n, p)
+    return round(Tserial(n)/Tparallel(n, p), 4)
 
 # method to calculate efficiency
 def efficiency(n, p):
-    return Tserial(n)/(p * Tparallel(n, p))
+    return round(Tserial(n)/(p * Tparallel(n, p)), 4)
 
-print("Speedup: " + str(speedup(10, 1)))
-print("Speedup: " + str(speedup(10, 2)))
+# init the values used for the problem
+n_values = [10, 20, 40, 80, 160, 320]
+p_values = [1, 2, 4, 8, 16, 32, 64, 128]
+
+# init the pretty tables that will output the data
+speed = PrettyTable(['p\\n', '10', '20', '40', '80', '160', '320'])
+eff = PrettyTable(['p\\n', '10', '20', '40', '80', '160', '320'])
+
+# create the tables
+for p in p_values:
+    # init the rows with the current p value
+    speed_row = [p]
+    eff_row = [p]
+
+    # calculate all speed/eff values for each n and current p and add them to row
+    for n in n_values:
+        speed_row.append(speedup(n, p))
+        eff_row.append(efficiency(n, p))
+
+    # add the completed rows to the table
+    speed.add_row(speed_row)
+    eff.add_row(eff_row)
+
+# print the completed tables
+print("Speed table:")
+print(speed)
+print("\nEfficiency table:")
+print(eff)
